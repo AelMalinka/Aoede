9c19c9c4e0edc01eff8b0da68ee6026b00610c49
Michael Thomas (malinka) <malinka@entropy-development.com>
2017-08-20 14:18:59 -0400

    finish testing on windows

--- win/WindowsTest/main.cc
+++ win/WindowsTest/main.cc
@@ -4,22 +4,72 @@
 
 #include <cstdlib>
 #include <iostream>
-#include <windows.h>
 
 #include <Entropy/Aoede/Application.hh>
+#include <Entropy/Aoede/Sound.hh>
 
 using namespace std;
+using namespace Entropy;
 using namespace Entropy::Aoede;
+using namespace Entropy::Tethys;
 
-int APIENTRY WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
+class Usage :
+	public Task
+{
+	public:
+		Usage(const string &);
+		void operator () ();
+	private:
+		string _name;
+};
+
+class Application :
+	public Aoede::Application
+{
+	public:
+		Application(const int, char *[]);
+	private:
+		std::shared_ptr<Usage> _usage;
+		std::shared_ptr<Sound> _sound;
+};
+
+int main(int ArgC, char *ArgV[])
 {
 	try
 	{
+		::Application app(ArgC, ArgV);
+		app();
+
 		return EXIT_SUCCESS;
 	}
 	catch (exception &e)
 	{
-		MessageBox(nullptr, e.what(), "Error!", MB_ICONEXCLAMATION | MB_OK);
+		cerr << e << endl;
+
 		return EXIT_FAILURE;
 	}
+}
+
+::Application::Application(const int argc, char *argv[])
+	: Tethys::Application(argc, argv), Aoede::Application(argc, argv)
+{
+	if (ArgC() != 2) {
+		_usage = make_shared<Usage>(ArgV()[0]);
+
+		Add(*_usage);
+	}
+	else {
+		_sound = make_shared<Sound>(ArgV()[1]);
+
+		Add(*_sound);
+	}
+}
+
+Usage::Usage(const string &name)
+	: _name(name)
+{}
+
+void Usage::operator () ()
+{
+	cout << _name << ": file" << endl;
 }
