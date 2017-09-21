cbd6d525eaad61aefc6303db1c67063e65e6ab7c
Michael Thomas (malinka) <malinka@entropy-development.com>
2017-09-21 02:52:43 -0400

    add sound test

---
+++ test/basic/sound.cc
@@ -0,0 +1,17 @@
+/*	Copyright 2017 (c) Michael Thomas (malinka) <malinka@entropy-development.com>
+	Distributed under the terms of the GNU Affero General Public License v3
+*/
+
+#include "Application.hh"
+#include "Sound.hh"
+#include <gtest/gtest.h>
+
+using namespace Entropy::Aoede;
+using namespace testing;
+using namespace std;
+
+namespace {
+	TEST(Sound, Create) {
+		Sound s("data/sound.oga");
+	}
+}
